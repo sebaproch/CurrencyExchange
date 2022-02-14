6c7876ae339bd78bfea3f2843b983dca981d483a
sebaproch <sebaproch@gmail.com>
2022-02-14 17:20:38 +0100

    Initial commit

---
+++ src/main/java/CurrencyExchangeCalculatorPage.java
@@ -0,0 +1,103 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrencyExchangeCalculatorPage extends PageObject {
+    private WebDriverWait wait;
+    @FindBy(xpath = "//input[contains(@class, 'ng-not-empty')][@type='text']")
+    protected WebElement notEmptyInputBox;
+    @FindBy(xpath = "//input[contains(@class, 'ng-empty')][@type='text']")
+    protected WebElement emptyInputBox;
+    @FindBy(xpath = "//span[@class='dropup']")
+    protected WebElement dropup;
+    @FindBy(xpath = "//div[@class='dropup']//button[@id='countries-dropdown']//span[@class='caret']")
+    protected WebElement countrySelector;
+    @FindBy(xpath = "//ul[@class='dropdown-menu']//li//a//span")
+    protected WebElement popupSelector;
+
+    String xpathAllRows = "//table//tr[@class='ng-scope']";
+    String dropdownElement = "//ul[@class='dropdown-menu']//li//a";
+
+    public CurrencyExchangeCalculatorPage(WebDriver driver) {
+        super(driver);
+        wait = new WebDriverWait(driver, 7);
+    }
+
+    public void fillAmountBox(double quantity) {
+        wait.until(ExpectedConditions.visibilityOf(notEmptyInputBox));
+        emptyInputBox.sendKeys(String.valueOf(quantity));
+    }
+
+    public void clickDropup() {
+        wait.until(ExpectedConditions.visibilityOf(dropup));
+        dropup.click();
+    }
+
+    public void clickCountrySelector(String name) throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOf(countrySelector));
+        countrySelector.click();
+        wait.until(ExpectedConditions.visibilityOf(popupSelector));
+        List<WebElement> allElements = driver.findElements(By.xpath(dropdownElement));
+
+        for (WebElement li : allElements) {
+            String t = li.getText();
+            if (li.getText().contains(name)) {
+                System.out.println(t);
+                li.click();
+                break;
+            } else {
+                System.out.println("Not found such name");
+            }
+        }
+    }
+
+    public List<String> checkingAllDifferences() {
+        List<String> issues = new ArrayList<>();
+
+        Integer row = 1;
+        List<WebElement> allRows = driver.findElements(By.xpath(xpathAllRows));
+        for (WebElement tr : allRows) {
+            WebElement paysera = tr.findElement(By.xpath("./td[@data-title='Paysera rate']//span[@class='ng-binding']"));
+            double payseraAmount = 0;
+            if (paysera.isDisplayed()) {
+                payseraAmount = Double.parseDouble(paysera.getText().replaceAll(",", ""));
+            }
+
+            try {
+            WebElement firstBank = tr.findElement(By.xpath("./td[@data-title='mBank amount']//span[@class='ng-binding']"));
+            double firstBankAmount = 0;
+            if (firstBank.isDisplayed()) {
+                firstBankAmount = Double.parseDouble(firstBank.getText().replaceAll(",", ""));
+                if (firstBankAmount < payseraAmount) {
+                    WebElement differenceElement = tr.findElement(By.xpath("./td[@data-title='mBank amount']//span[contains(@class, 'other-bank-loss')]"));
+                    double differenceAmount = 0;
+                    if (firstBank.isDisplayed()) {
+                        String text = differenceElement.getText().replaceAll("\\s", "").replaceAll(",", "").replaceAll("[\\[\\](){}]", "");
+                        differenceAmount = Double.parseDouble(text);
+                        double differenceAmountRound = Math.round(differenceAmount * 100) / 100;
+                        double expected = firstBankAmount - payseraAmount;
+                        double expectedRound = Math.round(expected * 100) / 100;
+                        if (expectedRound != differenceAmountRound) {
+                            issues.add("Invalid Bank Loss for row: " + row);
+                        }
+                    } else {
+                        issues.add("Bank Loss is not presented for row: " + row);
+                    }
+                }
+            }
+            } catch (Exception e) {
+        }
+        row++;
+    }
+        return issues;
+    }
+}
+
+
+
+
---
+++ src/test/java/CurrencyExchangeCalculatorTest.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class CurrencyExchangeCalculatorTest {
+    private WebDriver driver;
+    CurrencyExchangeCalculatorPage page;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        System.setProperty("webdriver.chrome.driver",
+                Utils.CHROME_DRIVER_LOCATION);
+        driver = new ChromeDriver();
+        page = new CurrencyExchangeCalculatorPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.get(Utils.BASE_URL);
+    }
+
+    @Test(priority = 0)
+    public void fillAmountBoxTest1() throws Exception {
+        page.fillAmountBox(255);
+        Thread.sleep(3000);
+    }
+
+    @Test(priority = 1)
+    public void changeLocalizationTest2 () throws Exception {
+        page.clickDropup();
+        Thread.sleep(4000);
+        page.clickCountrySelector("Poland");
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 2)
+    public void representDifferenceInTextBoxTest3() throws Exception {
+        List<String> issues = page.checkingAllDifferences();
+        Assert.assertTrue(issues.isEmpty(), "Issues: " + Arrays.toString(issues.toArray()));
+    }
+
+    @AfterClass
+    public void afterClass() throws Exception {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
+
+
+
+
